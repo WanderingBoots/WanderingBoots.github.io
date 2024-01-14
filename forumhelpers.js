446f6cc34f428e0d16a03959397bf107e757722e
WanderingBoots <amstrauz@gmail.com>
2024-01-13 22:58:59 -0500

    Update forumhelpers.js

--- forumhelpers.js
+++ forumhelpers.js
@@ -2,111 +2,107 @@
 // utility functions
 //
 
+// the spoofed user post element
+let g_spoofedPost = null;
+
+// all of the user's posts on the current page
+let g_userPosts = [];
+
 // possible text for the spoofed user post
-//   (hard-coded options for the game to select from)
 const g_textOptions = [
-    "<p> This is some safe text! </p>",
-    "<p> No code injections here! </p>",
+    "<p> I can't wait! </p>",
+    "<p> oh no </p>",
     "<p> :) </p>",
 ];
 
-function getPostElementOfFrame(frame) {
-    return frame.parentElement.parentElement.parentElement.parentElement; // gross, I know
+// CSS defining a "shake" animation
+//   idea shamelessly stolen from https://stackoverflow.com/questions/73537320/
+const g_shakeAnimation = `
+@keyframes shake {
+    0% { transform: translate(1px, 1px) rotate(0deg); }
+    10% { transform: translate(-1px, -3px) rotate(-0.25deg); }
+    20% { transform: translate(-4px, 0px) rotate(0.25deg); }
+    30% { transform: translate(4px, 3px) rotate(0deg); }
+    40% { transform: translate(1px, -1px) rotate(0.25deg); }
+    50% { transform: translate(-1px, 3px) rotate(-0.25deg); }
+    60% { transform: translate(-4px, 1px) rotate(0deg); }
+    70% { transform: translate(4px, 1px) rotate(-0.25deg); }
+    80% { transform: translate(-1px, -1px) rotate(0.25deg); }
+    90% { transform: translate(1px, 3px) rotate(0deg); }
+    100% { transform: translate(1px, -3px) rotate(-0.25deg); }
 }
+.shake {
+    animation: shake 0.25s;
+    animation-iteration-count: infinite;
+}
+`
 
 async function fetchForumURL(url) {
-    const response = await window.fetch(url);
+    const response = await fetch(url);
     return response.text();
 }
 
-async function getPostByCurrentUser() {
-    const profileElem = document.getElementById("profile");
-    if (profileElem === null) return null;
+async function fetchLastPostByUser(profileURL) {
+    const recentsHTML = await fetchForumURL(profileURL + "/posts");
+    if (recentsHTML !== null) {
+        const parser = new DOMParser();
+        const threadURL = parser
+            .parseFromString(recentsHTML, "text/html")
+            .getElementsByTagName("tr")[1]
+            .getElementsByTagName("a")[0].href;
+        const threadHTML = await fetchForumURL(threadURL);
 
-    const parser = new DOMParser();
-
-    // retrieve the latest post on the "see all messages by user" page
-    const currentProfile = profileElem.getElementsByTagName("a")[0].href;
-    const recentsHTML = await fetchForumURL(currentProfile + "/posts");
-    if (recentsHTML == null) return null;
-
-    const threadURL = parser
-        .parseFromString(recentsHTML, "text/html")
-        .getElementsByTagName("tr")[1]
-        .getElementsByTagName("a")[0].href;
-    const threadHTML = await fetchForumURL(threadURL);
-    if (threadHTML == null) return null;
-
-    const otherThread = parser.parseFromString(threadHTML, "text/html");
-
-    // find the user's post in the thread page
-    let userPost = null;
-    for (const post of otherThread.getElementsByClassName("post")) {
-        const profile = post.getElementsByClassName("member")[0].href;
-        const name = profile.slice(profile.lastIndexOf("/"));
-        if (currentProfile.endsWith(name)) {
-            userPost = post;
-            break;
+        if (threadHTML !== null) {
+            // find the user's post in the thread page
+            const otherThread = parser.parseFromString(threadHTML, "text/html");
+            for (const post of otherThread.getElementsByClassName("post")) {
+                const profile = post.getElementsByClassName("member")[0].href;
+                const name = profile.slice(profile.lastIndexOf("/"));
+                if (profileURL.endsWith(name)) {
+                    return post.cloneNode(true);
+                }
+            }
         }
-    }
-    if (userPost == null) return null;
-
-    // strip the links out of the edit/quote/report/etc buttons
-    const utils = userPost.getElementsByClassName("utils")[0];
-    for (const elem of utils.getElementsByTagName("a")) {
-        elem.href = "#";
-    }
-
-    // return the post element
-    return userPost.cloneNode(true);
-}
-
-async function insertUserPost() {
-    // ensure we haven't already done this
-    if (document.getElementsByClassName("avsdoda").length > 0) return;
-
-    // find an exemplar post by the logged-on user
-    const post = await getPostByCurrentUser();
-    if (post == null) return;
-
-    // assign "post even" to the post - post is inserted right after the OP
-    //   also assign "avsdoda" so it's faster/easier to find again
-    post.classList.add("post", "even", "avsdoda");
-
-    // replace the message-content with an initial value
-    post.getElementsByClassName("message-content")[0].innerHTML = g_textOptions[0];
-
-    // insert the faked user post as a reply to the OP
-    const op = document.getElementsByClassName("post")[0];
-    const inserted = op.parentElement.insertBefore(post, op.nextElementSibling);
-
-    // fix even/odd colors
-    let nextPost = inserted.nextElementSibling;
-    while (nextPost !== null) {
-        if (!nextPost.classList.replace("even", "odd")) {
-            nextPost.classList.replace("odd", "even");
-        }
-        nextPost = nextPost.nextElementSibling;
-    }
-
-    return inserted;
-}
-
-function getInsertedUserPost() {
-    // "avsdoda" tag was added to the inserted post
-    const avsdodas = document.getElementsByClassName("avsdoda");
-    if (avsdodas.length > 0) {
-        return avsdodas[0];
     }
     return null;
 }
 
-function getUserBadges() {
-    const userPost = getInsertedUserPost();
-    if (userPost !== null) {
-        return userPost.getElementsByClassName("badges")[0].children;
+async function findPostsByCurrentUser() {
+    const profileElem = document.getElementById("profile");
+    if (profileElem !== null) {
+        const currentProfile = profileElem.getElementsByTagName("a")[0].href;
+
+        g_userPosts = [];
+        for (const post of document.getElementsByClassName("post")) {
+            const otherProfile = post.getElementsByClassName("member")[0].href;
+            if (otherProfile == currentProfile) {
+                g_userPosts.push(post);
+            }
+        }
+        // if the user has no posts on this page, we have to find an exemplar
+        //   quick-reply is technically a post element, so ignore it here
+        if (g_userPosts.length == 0 || g_userPosts[0].classList.contains("quick")) {
+            const newPost = await fetchLastPostByUser(currentProfile);
+            if (newPost !== null) {
+                g_spoofedPost = newPost;
+            }
+        }
+        // otherwise just clone the first post we found
+        else {
+            g_spoofedPost = g_userPosts[0].cloneNode(true);
+        }
     }
-    return [];
+}
+
+function getUserBadges() {
+    let badges = []
+    if (g_spoofedPost !== null) {
+        for (const badge of g_spoofedPost.getElementsByClassName("badges")[0].children) {
+            const src = badge.getElementsByTagName("img")[0].src;
+            badges.push(src.split("/").reverse()[0]);
+        }
+    }
+    return badges;
 }
 
 //
@@ -117,20 +113,25 @@ function getUserBadges() {
 //
 
 async function doHello(frame, messageContent) {
-    await insertUserPost();
+    // inject shake animation CSS as a <style> tag
+    const style = document.createElement("style");
+    style.innerHTML = g_shakeAnimation;
+    document.head.appendChild(style);
 
-    const badgeElems = getUserBadges();
-    const post = getPostElementOfFrame(frame);
+    await findPostsByCurrentUser();
+
+    const badges = getUserBadges();
+    const post = frame.parentElement.parentElement.parentElement.parentElement; // gross, I know
     const profile = document.getElementById("profile");
     const username = profile !== null ? profile.innerText.trim() : null;
-	const iframeheight = frame.height;
 
     const pageInfo = {
-        badgecount: badgeElems.length,
+        badges: badges,
         bgcolor: getComputedStyle(post).backgroundColor,
+        frameheight: frame.height,
         postid: post.id,
         username: username,
-		iframeheight: iframeheight,
+        weezer: badges.includes("Weezerfestbadge.png"),
     };
     return pageInfo;
 }
@@ -141,20 +142,73 @@ async function doResize(frame, messageContent) {
 }
 
 async function doDeleteBadge(frame, messageContent) {
-    const badgeElems = getUserBadges();
-    if (badgeElems.length > 0) {
-        badgeElems[Math.floor(Math.random() * badgeElems.length)].remove();
+    for (const post of g_userPosts) {
+        for (let badge of post.getElementsByClassName("badges")[0].children) {
+            const src = badge.getElementsByTagName("img")[0].src;
+            if (src.endsWith(messageContent.name)) {
+                badge.remove();
+                break;
+            }
+        }
+    }
+}
+
+async function doDummyPost(frame, messageContent) {
+    if (g_spoofedPost !== null)
+    {
+        // ensure we haven't already done this
+        if (document.getElementsByClassName("avsdoda").length > 0) return;
+
+        // assign "post even" to the post - post is inserted right after the OP
+        //   also assign "avsdoda" so it's faster/easier to find again
+        g_spoofedPost.classList.add("post", "even", "avsdoda");
+
+        // replace the message-content with an initial value
+        g_spoofedPost.getElementsByClassName("message-content")[0].innerHTML = g_textOptions[0];
+
+        // strip the links out of the edit/quote/report/etc buttons
+        const utils = g_spoofedPost.getElementsByClassName("utils")[0];
+        for (const elem of utils.getElementsByTagName("a")) {
+            elem.href = "#";
+        }
+
+        // insert the faked user post as a reply to the OP
+        const op = document.getElementsByClassName("post")[0];
+        const inserted = op.parentElement.insertBefore(g_spoofedPost, op.nextElementSibling);
+        g_userPosts.splice(0, 0, g_spoofedPost);
+
+        // fix even/odd colors
+        let nextPost = inserted.nextElementSibling;
+        while (nextPost !== null) {
+            if (!nextPost.classList.replace("even", "odd")) {
+                nextPost.classList.replace("odd", "even");
+            }
+            nextPost = nextPost.nextElementSibling;
+        }
     }
 }
 
 async function doSetText(frame, messageContent) {
-    const post = getInsertedUserPost();
-    if (post !== null) {
+    if (g_spoofedPost !== null) {
         const idx = messageContent.index;
         if (idx < g_textOptions.length) {
-            const content = post.getElementsByClassName("message-content")[0];
+            const content = g_spoofedPost.getElementsByClassName("message-content")[0];
             content.innerHTML = g_textOptions[idx];
         }
+    }
+}
+
+async function doShakeStart(frame, messageContent) {
+    // frame.classList.add("shake");
+    for (const elem of document.getElementsByTagName("*")) {
+        elem.classList.add("shake");
+    }
+}
+
+async function doShakeStop(frame, messageContent) {
+    // frame.classList.remove("shake");
+    for (const elem of document.getElementsByTagName("*")) {
+        elem.classList.remove("shake");
     }
 }
 
@@ -165,9 +219,12 @@ async function messageHandler(event) {
     // each command handler uses the same function prototype
     const messageTypes = {
         hello: doHello,
-        resize: doResize,
         delbadge: doDeleteBadge,
+        dummypost: doDummyPost,
+        resize: doResize,
         settext: doSetText,
+        shakestart: doShakeStart,
+        shakestop: doShakeStop,
     };
 
     // locate the iframe source
@@ -188,7 +245,7 @@ async function messageHandler(event) {
 }
 
 async function registerListener() {
-    window.addEventListener("message", (event) => messageHandler(event), false);
+    addEventListener("message", (event) => messageHandler(event), false);
 }
 
 // TODO: would ideally like this to instead live in the `onload` attribute of the iframe tag
